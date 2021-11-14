ae6a3b8ebd3f5a1a3287cfb84227f54cf8f83774
Ós Córdova <zeptok@gmail.com>
2021-11-14 08:17:21 -0600

    💄 Add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h1 className="text-5xl">Tailwind CSS</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
